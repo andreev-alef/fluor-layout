ab53256dd4b8616c4ccfb54947ce47f41b77cc6e
andreev-alef <alef-makeup@yandex.ru>
2015-12-08 13:10:06 +0700

    Первая фиксация

---
+++ src/java/index/LoginPageBean.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package index;
+
+import java.io.Serializable;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author alef
+ */
+@Named(value = "loginPageBean")
+@ViewScoped
+public class LoginPageBean implements Serializable {
+
+    private String buttonCaption;
+
+    public String getButtonCaption() {
+        try {
+            buttonCaption = "ВОЙТИ <span class=\"glyphicon glyphicon-log-in\"></span>";
+
+        } catch (Exception e) {
+        }
+        return buttonCaption;
+    }
+
+    /**
+     * Creates a new instance of LoginPageBean
+     */
+    public LoginPageBean() {
+
+    }
+
+}
